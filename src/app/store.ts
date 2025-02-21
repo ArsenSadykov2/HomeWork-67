7e9eb3121250f70a9ccdf7e25e3fe03607b6d53e
Sadykov Arsen <LoCo10291998@gmail.com>
2025-02-21 17:08:53 +0600

    Commit. created a file for storage

---
+++ src/app/store.ts
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import passwordReducer from '../containers/CounterSlice.ts';
+
+export const store = configureStore({
+    reducer: {
+        password: passwordReducer,
+    },
+});
